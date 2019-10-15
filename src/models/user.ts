1b1bed3607626705b0c7a211f18c0f7e2e005ed6
937931249 <937931249@qq.com>
2019-10-16 00:27:54 +0800

    Init demo with umi

---
+++ src/models/user.ts
@@ -0,0 +1,18 @@
+export default {
+  namespace: 'user',
+  state: {},
+  subscriptions: {
+    setup({ dispatch, history }) {},
+  },
+  effects: {
+    *fetch({ payload }, { call, put }) {
+      yield put({ type: 'save' });
+    },
+  },
+
+  reducers: {
+    save(state, action) {
+      return { ...state, ...action.payload };
+    },
+  },
+};
