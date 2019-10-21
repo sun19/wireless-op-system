839936c9c863dabd0c933a7131c8609a509c19d2
杨冬林 <1936881152@qq.com>
2019-10-21 23:34:25 +0800

    登录界面接口

--- src/pages/login/components/LoginForm.tsx
+++ src/pages/login/components/LoginForm.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Form, Icon, Input, Row, Col, Radio } from 'antd';
+import { Form, Icon, Input, Row, Col, Radio, Button } from 'antd';
 import { ICON_FONTS_URL } from '../../../config/constants';
 import styles from '../index.css';
 
@@ -9,15 +9,48 @@ const IconFont = Icon.createFromIconfontCN({
 });
 
 class NormalLoginForm extends React.Component {
+  constructor(props: any) {
+    super(props);
+    // this.state={
+    //   userName:'',
+    //   passWord:'',
+    // }
+  }
   handleSubmit = e => {
     e.preventDefault();
     this.props.form.validateFields((err, values) => {
       if (!err) {
-        console.log('Received values of form: ', values);
+        let data = {
+          username: values.username,
+          password: values.password,
+        };
+        var myInit = {
+          method: 'GET',
+          url: `http://47.96.112.31:8085/jeecg-boot/intf/location/login?username=jeecg&password=123456`,
+          // headers: {
+          //   'Content-Type': 'image/jpeg'
+          // },
+          // mode: 'cors',
+          // cache: 'default'
+        };
+        fetch().then(function(response) {
+
+        });
+        // var myRequest = new Request(data, myInit);
+        // console.log('Received values of form: ', values);
       }
     });
   };
-
+  // loginBtn=()=>{
+  //   console.log('2',this.props.form.validateFields)
+  //   this.props.form.validateFields((err, values) => {
+  //     console.log(values)
+  //     if (!err) {
+  //       console.log('Received values of form: ', values);
+  //     }
+  //   });
+  //   console.log('ffff1')
+  // }
   render() {
     const { getFieldDecorator } = this.props.form;
     return (
@@ -48,11 +81,14 @@ class NormalLoginForm extends React.Component {
           </Form.Item>
           <Form.Item>
             {/* <Button type="primary" htmlType="submit" className="login-form-button">
-              登陆
+              登录
             </Button> */}
             <Row type="flex" justify="center" style={{ height: '100%' }}>
               <Col span={12}>
-                <div className={styles.login_btn}>登陆</div>
+                {/* className={styles.login_btn} onClick={this.loginBtn()} */}
+                <Button className={styles.login_btn} onClick={this.handleSubmit}>
+                  登录
+                </Button>
               </Col>
             </Row>
           </Form.Item>
