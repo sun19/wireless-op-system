6781f5570f841a10b8ddbfaed909bc3c5e999859
937931249 <937931249@qq.com>
2019-10-23 23:55:18 +0800

    添加ws获取数据

--- src/pages/map-manager/index.tsx
+++ src/pages/map-manager/index.tsx
@@ -19,21 +19,25 @@ interface Props {
 interface Lamp {
   x: number;
   y: number;
+  id: string;
 }
-
+/**
+ * 默认灯的位置以及灯的编号
+ */
 const defaultLamps = [
-  { x: 1190, y: 435 },
-  { x: 1190, y: 562 },
-  { x: 1190, y: 698 },
-  { x: 1190, y: 815 },
-  { x: 1337, y: 435 },
-  { x: 1337, y: 562 },
-  { x: 1337, y: 688 },
-  { x: 1337, y: 815 },
+  { x: 1190, y: 435, id: '08' },
+  { x: 1190, y: 562, id: '07' },
+  { x: 1190, y: 698, id: '06' },
+  { x: 1190, y: 815, id: '05' },
+  { x: 1337, y: 435, id: '01' },
+  { x: 1337, y: 562, id: '02' },
+  { x: 1337, y: 688, id: '03' },
+  { x: 1337, y: 815, id: '04' },
 ];
 
 export default class MapManager extends React.Component<Props, State> {
   map: React.RefObject<HTMLDivElement>;
+  ws: WebSocket;
   constructor(props) {
     super(props);
     this.map = React.createRef<HTMLDivElement>();
@@ -42,7 +46,7 @@ export default class MapManager extends React.Component<Props, State> {
       icon: null,
       width: 0,
       height: 0,
-      lamps: defaultLamps,
+      lamps: [],
     };
   }
 
@@ -51,14 +55,32 @@ export default class MapManager extends React.Component<Props, State> {
     const mapImage = await this.dynamicLoadMapImage();
     const iconImage = await this.dynamicLoadIconImage();
     const { clientWidth, clientHeight } = this.map.current;
-    const currentLamps = this.setupLampData(this.state.lamps, clientWidth, clientHeight);
+    this.connectWs();
     this.setState({
       mapImage,
       icon: iconImage,
       width: clientWidth,
       height: clientHeight,
-      lamps: currentLamps,
     });
+  }
+
+  connectWs() {
+    const { clientWidth, clientHeight } = this.map.current;
+    this.ws = new WebSocket('ws://47.96.112.31:8084/jeecg-boot/websocket/1');
+    this.ws.onopen = () => {};
+
+    this.ws.onmessage = evt => {
+      const message = JSON.parse(evt.data);
+      const msgText = message.msgTxt;
+      const lamp = { x: +msgText.xCoordinate, y: +msgText.yCoordinate, id: msgText.lampNumber };
+      const currentLamps = this.setupLampData([lamp], clientWidth, clientHeight);
+
+      this.setState({
+        lamps: currentLamps,
+      });
+    };
+
+    this.ws.onclose = () => {};
   }
 
   setupLampData = (data, currentWidth, currentHeight) => {
