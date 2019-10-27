8620fef406337af0de66068fc63f21dca5fb7c19
杨冬林 <1936881152@qq.com>
2019-10-27 16:31:20 +0800

    路由设置可配置

--- src/components/LeftMenuList.tsx
+++ src/components/LeftMenuList.tsx
@@ -1,21 +1,23 @@
 import React, { Component } from 'react';
 import { Menu, Icon } from 'antd';
 import { ICON_FONTS_URL } from '../config/constants';
-// import { Menus } from '../config/menus';
-
+import { leftMenus } from '../config/menus';
 import router from 'umi/router';
-
+import Link from 'umi/link';
 import styles from './leftMenuList.less';
-
+const leftMenu = leftMenus;
 const { SubMenu } = Menu;
 const IconFont = Icon.createFromIconfontCN({
   scriptUrl: ICON_FONTS_URL,
 });
+let rootKeys = leftMenu.map(item => item.name);
+
 class LeftMenuList extends Component {
-  rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+  // rootSubmenuKeys = ['sub1', 'sub2', 'sub4'];
+  rootSubmenuKeys = rootKeys;
   showUp = false;
   state = {
-    openKeys: ['sub1'],
+    openKeys: [rootKeys[0]],
     current: '1',
   };
   handleClick = value => {
@@ -23,11 +25,19 @@ class LeftMenuList extends Component {
     this.setState({
       current: value.key,
     });
-    //  router.push('/index');
+    let leftpath = leftMenu.find(() => {
+      return { name: value.key };
+    });
+      this.setState({
+        openKeys: [value.keyPath[1]],
+      });
+      // console.log(this.state.openKeys);
+    
   };
   onOpenChange = (openKeys: any) => {
     this.showUp = openKeys.length > 0 ? true : false;
     const latestOpenKey = openKeys.find((key: any) => this.state.openKeys.indexOf(key) === -1);
+    // console.log(latestOpenKey, this.rootSubmenuKeys.indexOf(latestOpenKey) === -1);
     if (this.rootSubmenuKeys.indexOf(latestOpenKey) === -1) {
       this.setState({ openKeys });
     } else {
@@ -38,50 +48,47 @@ class LeftMenuList extends Component {
   };
 
   render() {
+      // console.log(this.state.openKeys);
     return (
       <Menu
         mode="inline"
-        defaultOpenKeys={['sub1']}
+        defaultOpenKeys={[rootKeys[0]]}
         onClick={this.handleClick}
         selectedKeys={[this.state.current]}
         openKeys={this.state.openKeys}
-        // onOpenChange={this.onOpenChange}
+        onOpenChange={this.onOpenChange}
         style={{ width: 256 }}
         className={[`${styles.no_background}`, `${styles.menu_bar}`].join(' ')}
       >
-        {/* <For >
-
-        </For> */}
-        <SubMenu
-          key="sub1"
-          className={[`${styles.no_background}`, `${styles.sub_menu_list}`].join(' ')}
-          title={
-            <span>
-              <Icon type="setting" />
-              <span>大屏展示</span>
-              <IconFont
-                className={`${styles.icon_down}`}
-                type={this.showUp === false ? 'icon-down1' : 'icon-up1'}
-              />
-            </span>
-          }
-        >
-          <Menu.Item key="1">
-            <span className={`${styles.menu_item}`}>
-              {/* `${styles.menu_item_underline_active}` */}
-              大屏展示
-            </span>
-          </Menu.Item>
-          <Menu.Item key="2">
-            <span className={[`${styles.menu_item}`].join(' ')}>数据</span>
-          </Menu.Item>
-          <Menu.Item key="3" className={styles.menu_item}>
-            <span className={[`${styles.menu_item}`].join(' ')}>主界面</span>
-          </Menu.Item>
-          <Menu.Item key="4" className={styles.menu_item}>
-            <span className={[`${styles.menu_item}`].join(' ')}>应用</span>
-          </Menu.Item>
-        </SubMenu>
+        {leftMenu.map(leftMenuItem => {
+          return (
+            <SubMenu
+              key={leftMenuItem.name}
+              className={[`${styles.no_background}`, `${styles.sub_menu_list}`].join(' ')}
+              title={
+                <span>
+                  <Icon type="setting" />
+                  <span className={`${styles.icon_title}`}>{leftMenuItem.name}</span>
+                  <IconFont
+                    className={`${styles.icon_down}`}
+                    type={this.state.openKeys[0] == leftMenuItem.name ? 'icon-down1' : 'icon-up1'}
+                  />
+                </span>
+              }
+            >
+              {leftMenuItem.children && leftMenuItem.children.length > 0
+                ? leftMenuItem.children.map(childrens => {
+                    return (
+                      <Menu.Item key={childrens.name}>
+                        <Link className={`${styles.menu_item}`} to={childrens.path}>{childrens.name}</Link>
+                        {/* <span className={`${styles.menu_item}`}>{childrens.name}</span> */}
+                      </Menu.Item>
+                    );
+                  })
+                : ''}
+            </SubMenu>
+          );
+        })}
       </Menu>
     );
   }
--- src/config/menus.ts
+++ src/config/menus.ts
@@ -1,163 +1,193 @@
 
-export const Menus = [
+export const leftMenus = [
+    // {
+    //     name: 'login',
+    //     path: '/',
+    //     // component: './pages/index.tsx'
+    // },
     {
-        name: 'login',
-        path: '/',
-        // component: './pages/index.js'
-    },
-    {
-        name:'系统设置',
-        path: 'system-setting',
+        name: '系统设置',
+        key: '01',
+        path: '',
         component: './pages/system-setting',
-        children:[
+        children: [
             {
                 name: '用户管理',
-                path: 'system-setting/customer-manager',
-                component: './pages/system-setting/customer-manager/index.js'
+                key: '02',
+                path: '/system-setting/customer-manager',
+                component: './pages/system-setting/customer-manager/index.tsx'
             }, {
                 name: '人员类型',
-                path: 'system-setting/people-type',
-                component: './pages/system-setting/people-type/index.js'
+                key: '03',
+                path: '/system-setting/people-type',
+                component: './pages/system-setting/people-type/index.tsx'
             },
             {
                 name: '信息牌设置',
-                path: 'system-setting/message-card',
-                component: './pages/system-setting/message-card/index.js'
+                key: '04',
+                path: '/system-setting/message-card',
+                component: './pages/system-setting/message-card/index.tsx'
             },
             {
                 name: '超级管理员设置',
-                path: 'system-setting/super-admin',
-                component: './pages/system-setting/super-admin/index.js'
+                key: '05',
+                path: '/system-setting/super-admin',
+                component: './pages/system-setting/super-admin/index.tsx'
             },
         ]
     },
-   {
+    {
         name: '人员管理',
-        path: 'user-manager',
+        key: '06',
+        path: '/user-manager',
         component: './pages/user-manager',
         children: [
             {
                 name: '用户列表-内部',
-                path: 'user-manager/user-inside',
-                component: './pages/user-manager/user-inside/index.js'
+                key: '07',
+                path: '/user-manager/user-inside',
+                component: './pages/user-manager/user-inside/index.tsx'
             }, {
                 name: '用户列表-外部',
-                path: 'user-manager/user-outside',
-                component: './pages/user-manager/user-outside/index.js'
+                key: '08',
+                path: '/user-manager/user-outside',
+                component: './pages/user-manager/user-outside/index.tsx'
             },
         ]
     },
     {
         name: '信息牌管理',
-        path: 'info-card-manager',
+        key: '09',
+        path: '/info-card-manager',
         component: './pages/info-card-manager',
         children: [
             {
                 name: '信息牌列表',
-                path: 'info-card-manager/info-card-list',
-                component: './pages/info-card-manager/info-card-list/index.js'
+                key: '10',
+                path: '/info-card-manager/info-card-list',
+                component: './pages/info-card-manager/info-card-list/index.tsx'
             }, {
                 name: '任务规划',
-                path: 'info-card-manager/task-planning',
-                component: './pages/info-card-manager/task-planning/index.js'
+                key: '11',
+                path: '/info-card-manager/task-planning',
+                component: './pages/info-card-manager/task-planning/index.tsx'
             },
         ]
     }, {
         name: '地图管理',
-        path: 'map-manager',
+        key: '12',
+        path: '/map-manager',
         component: './pages/map-manager',
         children: [
             {
                 name: '地图导入',
-                path: 'map-manager/map-import',
-                component: './pages/map-manager/map-import/index.js'
+                key: '13',
+                path: '/map-manager/map-import',
+                component: './pages/map-manager/map-import/index.tsx'
             }, {
                 name: '区域设置',
-                path: 'map-manager/area-set',
-                component: './pages/map-manager/area-set/index.js'
+                key: '14',
+                path: '/map-manager/area-set',
+                component: './pages/map-manager/area-set/index.tsx'
             }, {
                 name: '电子围栏设置',
-                path: 'map-manager/fence-setting',
-                component: './pages/map-manager/fence-setting/index.js'
+                key: '15',
+                path: '/map-manager/fence-setting',
+                component: './pages/map-manager/fence-setting/index.tsx'
             }, {
                 name: '灯具设置',
-                path: 'map-manager/lamps-set',
-                component: './pages/map-manager/lamps-set/index.js'
+                key: '16',
+                path: '/map-manager/lamps-set',
+                component: './pages/map-manager/lamps-set/index.tsx'
             }, {
                 name: '巡检点设置',
-                path: 'map-manager/polling-point',
-                component: './pages/map-manager/polling-point/index.js'
+                key: '17',
+                path: '/map-manager/polling-point',
+                component: './pages/map-manager/polling-point/index.tsx'
             }, {
                 name: '巡检路线设置',
-                path: 'map-manager/polling-line',
-                component: './pages/map-manager/polling-line/index.js'
+                key: '18',
+                path: '/map-manager/polling-line',
+                component: './pages/map-manager/polling-line/index.tsx'
             },
         ]
     }, {
         name: '告警管理',
-        path: 'warning-manager',
+        key: '19',
+        path: '/warning-manager',
         component: './pages/warning-manager',
         children: [
             {
                 name: '告警类型',
-                path: 'warning-manager/type',
-                component: './pages/warning-manager/type/index.js'
+                key: '20',
+                path: '/warning-manager/type',
+                component: './pages/warning-manager/type/index.tsx'
             }, {
                 name: '告警信息',
-                path: 'warning-manager/info',
-                component: './pages/warning-manager/info/index.js'
+                key: '21',
+                path: '/warning-manager/info',
+                component: './pages/warning-manager/info/index.tsx'
             }, {
                 name: '历史告警',
-                path: 'warning-manager/history',
-                component: './pages/warning-manager/history/index.js'
+                key: '22',
+                path: '/warning-manager/history',
+                component: './pages/warning-manager/history/index.tsx'
             },
         ]
     }, {
         name: '巡检管理',
-        path: 'route-inspect-manager',
+        key: '23',
+        path: '/route-inspect-manager',
         component: './pages/route-inspect-manager',
         children: [
             {
                 name: '巡检列表',
-                path: 'route-inspect-manager/route-inspect-list',
-                component: './pages/route-inspect-manager/route-inspect-list/index.js'
+                key: '24',
+                path: '/route-inspect-manager/route-inspect-list',
+                component: './pages/route-inspect-manager/route-inspect-list/index.tsx'
             }, {
                 name: '巡检报表',
-                path: 'route-inspect-manager/route-inspect-report',
-                component: './pages/route-inspect-manager/route-inspect-report/index.js'
-            }, 
+                key: '25',
+                path: '/route-inspect-manager/route-inspect-report',
+                component: './pages/route-inspect-manager/route-inspect-report/index.tsx'
+            },
         ]
     }, {
         name: '日志管理',
-        path: 'log-manager',
+        key: '26',
+        path: '/log-manager',
         component: './pages/log-manager',
         children: [
             {
                 name: '日志列表',
-                path: 'log-manager/log-list',
-                component: './pages/log-manager/log-list/index.js'
-            },  
+                key: '27',
+                path: '/log-manager/log-list',
+                component: './pages/log-manager/log-list/index.tsx'
+            },
         ]
     }, {
         name: '大屏展示',
-        path: 'big-screen',
+        key: '28',
+        path: '/big-screen',
         component: './pages/big-screen',
         // children: [
         //     {
         //         name: '大屏展示',
         //         path: 'big-screen/info-card-list',
-        //         component: './pages/users/index.js'
+        //         component: './pages/users/index.tsx'
         //     },
         // ]
     }, {
         name: '统计查询',
-        path: 'statistics-query',
+        key: '29',
+        path: '/statistics-query',
         component: './pages/statistics-query',
         children: [
             {
                 name: '历史轨迹',
-                path: 'statistics-query/statistics-history',
-                component: './pages/statistics-query/statistics-history/index.js'
+                key: '30',
+                path: '/statistics-query/statistics-history',
+                component: './pages/statistics-query/statistics-history/index.tsx'
             },
         ]
     },
