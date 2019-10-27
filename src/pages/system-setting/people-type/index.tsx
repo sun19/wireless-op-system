15965930cd1957cd41fc765e9deabdb5bc49b42d
937931249 <937931249@qq.com>
2019-10-28 01:23:29 +0800

    修改form表单组件

--- src/pages/system-setting/people-type/index.tsx
+++ src/pages/system-setting/people-type/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
 
+import { MainContent } from '../components';
+
 const PeopelType: React.FC = props => {
-  return <div style={{ color: 'white' }}>人员类型</div>;
+  return (
+    <div style={{ color: 'white' }}>
+      <MainContent />
+    </div>
+  );
 };
 
 export default PeopelType;
