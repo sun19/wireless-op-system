0d8a1c530f1eb7b09d21386bd8308e05ff8c6c7c
937931249 <937931249@qq.com>
2019-10-28 01:46:17 +0800

    路由跳转

--- src/components/menuBreadCrumb.tsx
+++ src/components/menuBreadCrumb.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Breadcrumb } from 'antd';
+import NavLink from 'umi/navlink';
+import withBreadcrumbs from 'react-router-breadcrumbs-hoc';
 
 import styles from './menuBreadCrumb.less';
 
-export default class MenuBreadCrumb extends React.Component {
+export default class MenuBreadCrumb extends React.Component<any, any> {
   render() {
     return (
       <Breadcrumb separator=">" className={styles.bread_box}>
