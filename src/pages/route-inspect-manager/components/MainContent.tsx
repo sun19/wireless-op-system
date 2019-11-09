f03f1598d391b73910337a1149aca1a7dfabc129
杨冬林 <1936881152@qq.com>
2019-11-09 20:52:11 +0800

    修改 MainContent

--- src/pages/route-inspect-manager/components/MainContent.tsx
+++ src/pages/route-inspect-manager/components/MainContent.tsx
@@ -6,7 +6,7 @@ import EditableTable from '../../../components/EditorableTable';
 import ContentBorder from '../../../components/ContentBorder';
 
 @Form.create()
-export default class MainContent extends React.Component {
+export default class MainContent extends React.Component<any> {
   constructor(props: any) {
     super(props);
   }
@@ -16,7 +16,7 @@ export default class MainContent extends React.Component {
       <div className={styles.table_list}>
         <div className={styles.table_list_panel}>
           <ContentBorder>
-            <EditableFormTable />
+            <EditableFormTable {...this.props} />
           </ContentBorder>
         </div>
       </div>
