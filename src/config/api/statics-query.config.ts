844663b88fce0aef7b8793f3145be4909fb69d9f
杨冬林 <1936881152@qq.com>
2019-11-17 20:39:59 +0800

    统计查询-历史轨迹

--- src/config/api/statics-query.config.ts
+++ src/config/api/statics-query.config.ts
@@ -1,1 +1,11 @@
-export const static_query = '';
+// ----------------------统计查询：历史轨迹 ——————————————————————————————
+
+/**
+ * 获取超级管理员列表 
+ */
+export const GET_STATISTICS_HISTORY_LIST = 'http://47.96.112.31:8086/jeecg-boot/intf/location/listRoute';
+
+/**
+ * 获取所有用户角色
+ */
+export const GET_ALL_ROLES = 'http://47.96.112.31:8086/jeecg-boot/intf/location/getAllRole';
