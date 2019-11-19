4efc686ca2a7463c238c37778f2db1532a9f02ad
杨冬林 <1936881152@qq.com>
2019-11-19 23:07:08 +0800

    添加返回功能

--- src/pages/statistics-query/statistics-history/add.tsx
+++ src/pages/statistics-query/statistics-history/add.tsx
@@ -13,9 +13,7 @@ import styles from './index.less';
 const { TextArea } = Input;
 const { Option } = Select;
 
-
-
-interface FormProps extends FormComponentProps {}
+interface FormProps extends FormComponentProps { }
 
 type StateProps = ReturnType<typeof mapState>;
 type Props = StateProps & UmiComponentProps & FormProps;
@@ -30,73 +28,73 @@ class historyAdd extends React.Component<Props> {
   goBack = () => {
     this.props.form.resetFields();
     router.push('/statistics-query/statistics-history');
-  }; 
-render() {
+  };
+  render() {
     const props = this.props;
-  const { getFieldDecorator } = props.form;
-  return (
-    <ContentBorder className={styles.auth_root}>
-      <Form layout="inline" labelAlign="right" style={{ marginTop: '0.57rem' }}>
-        <div className="auth__inner--container">
-          <div className={styles.input_body}>
-            <Row type="flex" justify="space-between">
-              <Col span={24}>
-                <Form.Item label="信息牌">
-                  {getFieldDecorator('信息牌', {
-                    rules: [
-                      {
-                        message: '请输入信息牌',
-                      },
-                    ],
-                  })(<Input placeholder="请输入信息牌" />)}
-                </Form.Item>
-                <Form.Item label="姓名">
-                  {getFieldDecorator('姓名', {
-                    rules: [
-                      {
-                        message: '请输入姓名',
-                      },
-                    ],
-                  })(<Input placeholder="请输入姓名" />)}
-                </Form.Item>
-                <Form.Item label="类型">
-                  {getFieldDecorator('类型', {
-                    rules: [
-                      {
-                        message: '请输入类型',
-                      },
-                    ],
-                  })(<Input placeholder="请输入类型" />)}
-                </Form.Item>
-              </Col>
-            </Row>
+    const { getFieldDecorator } = props.form;
+    return (
+      <ContentBorder className={styles.auth_root}>
+        <Form layout="inline" labelAlign="right" style={{ marginTop: '0.57rem' }}>
+          <div className="auth__inner--container">
+            <div className={styles.input_body}>
+              <Row type="flex" justify="space-between">
+                <Col span={24}>
+                  <Form.Item label="信息牌">
+                    {getFieldDecorator('信息牌', {
+                      rules: [
+                        {
+                          message: '请输入信息牌',
+                        },
+                      ],
+                    })(<Input placeholder="请输入信息牌" />)}
+                  </Form.Item>
+                  <Form.Item label="姓名">
+                    {getFieldDecorator('姓名', {
+                      rules: [
+                        {
+                          message: '请输入姓名',
+                        },
+                      ],
+                    })(<Input placeholder="请输入姓名" />)}
+                  </Form.Item>
+                  <Form.Item label="类型">
+                    {getFieldDecorator('类型', {
+                      rules: [
+                        {
+                          message: '请输入类型',
+                        },
+                      ],
+                    })(<Input placeholder="请输入类型" />)}
+                  </Form.Item>
+                </Col>
+              </Row>
 
-            <Row className={styles.line_style}>
-              <Col className={styles.line_type} span={11} />
-              <Col span={2}>地图</Col>
-              <Col className={styles.line_type} span={11} />
-            </Row>
-            <Row className={styles.line_style}>
-              <Col className={styles.img_type} span={24} />
-            </Row>
-            <Row type="flex" justify="center" style={{ marginTop: '0.35rem' }}>
-              <Col span={2}>
-                <Form.Item className={styles.button_type}>
-                  <Button className={styles.form_btn}>确认</Button>
-                </Form.Item>
-              </Col>
-              <Col span={2} className={styles.select_padding_left}>
-                <Form.Item>
-                  <Button className={styles.form_btn} onClick={this.goBack}>返回</Button>
-                </Form.Item>
-              </Col>
-            </Row>
+              <Row className={styles.line_style}>
+                <Col className={styles.line_type} span={11} />
+                <Col span={2}>地图</Col>
+                <Col className={styles.line_type} span={11} />
+              </Row>
+              <Row className={styles.line_style}>
+                <Col className={styles.img_type} span={24} />
+              </Row>
+              <Row type="flex" justify="center" style={{ marginTop: '0.35rem' }}>
+                <Col span={2}>
+                  <Form.Item className={styles.button_type}>
+                    <Button className={styles.form_btn}>确认</Button>
+                  </Form.Item>
+                </Col>
+                <Col span={2} className={styles.select_padding_left}>
+                  <Form.Item>
+                    <Button className={styles.form_btn} onClick={this.goBack}>返回</Button>
+                  </Form.Item>
+                </Col>
+              </Row>
+            </div>
           </div>
-        </div>
-      </Form>
-    </ContentBorder>
-  );
-}
+        </Form>
+      </ContentBorder>
+    );
+  }
 };
 
 const HistoryAdd = Form.create<Props>({ name: 'history_add' })(historyAdd);
@@ -106,7 +104,7 @@ const mapState = ({ statisticsQuery }) => {
   // const { allDuties, allSecretLevel } = commonState;
   return {
     history: resp,
-    
+
   };
 };
 
