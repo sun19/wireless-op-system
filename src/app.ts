516e3e7ae5146c6722e9de1a6ca7190b5508b037
937931249 <937931249@qq.com>
2019-12-04 21:05:02 +0800

    删除持久化store

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,3 @@
-import { persistStore, persistReducer } from 'redux-persist';
-import storage from 'redux-persist/lib/storage';
-
 (function (doc, win) {
   var docEl = doc.documentElement,
     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
@@ -14,23 +11,10 @@ import storage from 'redux-persist/lib/storage';
   doc.addEventListener('DOMContentLoaded', recalc, false);
 })(document, window);
 
-const persistConfig = {
-  key: 'root',
-  storage,
-};
-
-const persistEnhancer = () => createStore => (reducer, initialState, enhancer) => {
-  const store = createStore(persistReducer(persistConfig, reducer), initialState, enhancer);
-  const persist = persistStore(store, null);
-  return { ...store, persist };
-};
-
 export const dva = {
   config: {
     onError(err: ErrorEvent) {
       err.preventDefault();
     },
-    extraEnhancers: [persistEnhancer()],
-    onReducer: reducer => persistReducer(persistConfig, reducer),
   },
 };
