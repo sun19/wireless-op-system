e4f44344fa497d024372524be54ea3e0ed5aa22b
937931249 <937931249@qq.com>
2019-12-08 01:27:24 +0800

    修改菜单接口

--- src/config/api/login.config.ts
+++ src/config/api/login.config.ts
@@ -7,7 +7,7 @@ export const LOGIN_OUT = `${BASE_API_URL}/jeecg-boot/intf/location/logout`;
 // 获取左侧菜单
 export const LEFT_MENU_LIST = `${BASE_API_URL}/jeecg-boot/intf/location/queryMenuAndParentListByRoleId`
 // 根据当前登录用户查询拥有的菜单key
-export const MENU_LIST = `${BASE_API_URL}/jeecg-boot/intf/location/queryMenuListByUserId`;
+export const MENU_LIST = `${BASE_API_URL}/jeecg-boot/intf/location/listMenuByRoleId`;
 // 查询所有菜单
 export const PEOPLE_MENUS = `${BASE_API_URL}/jeecg-boot/intf/location/queryMenuList`;
 // 编辑菜单权限
