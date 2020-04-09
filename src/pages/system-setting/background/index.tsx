f36083fd6a2213445ccfcfe44e1e4c7a7a00d62d
StuRuby <937931249@qq.com>
2020-04-10 00:33:27 +0800

    feature:新增背景图上产

--- src/pages/system-setting/background/index.tsx
+++ src/pages/system-setting/background/index.tsx
@@ -2,7 +2,7 @@
  * title: 设置 > 高级管理员设置 > 基础数据设置 > 信息牌背景设置
  */
 import React from 'react';
-import { Layout, message } from 'antd';
+import { Layout, message, Button, Icon, Upload } from 'antd';
 import { connect } from 'dva';
 import router from 'umi/router';
 import * as _ from 'lodash';
@@ -11,6 +11,8 @@ import MainContent from '../components/MainContent';
 import { UmiComponentProps } from '@/common/type';
 import { getCompanyNameList, updateSuperAdmin, uploadImg } from '../services';
 import publicStyles from '../index.less';
+import backgroundStyles from './index.less';
+import { UPLOAD_IMG } from '@/config/api';
 
 const { Content } = Layout;
 type StateProps = ReturnType<typeof mapState>;
@@ -18,43 +20,75 @@ type Props = StateProps & UmiComponentProps;
 interface State {
   type: string;
   remark: string;
-  fileData: string
+  fileData: string;
+  message: string;
 }
 
+const props = {};
+
 class SuperAdmin extends React.Component<Props, State> {
+  uploadProps: any;
   constructor(props: any) {
     super(props);
     this.state = {
       type: '',
       remark: '',
-      fileData: ''
+      fileData: '',
+      message: '',
     };
 
     this.uploadImg = this.uploadImg.bind(this);
+    this.uploadProps = {
+      name: 'file',
+      action: UPLOAD_IMG,
+      headers: {
+        authorization: 'authorization-text',
+      },
+      onChange(info) {
+        if (info.file.status !== 'uploading') {
+          // console.log(info.file, info.fileList, 'uploading');
+        }
+        if (info.file.status === 'done') {
+          message.success(`${info.file.name} file uploaded successfully`);
+          let resp = info.response || '{}';
+          resp = JSON.parse(resp);
+          const txt = resp.message || '';
+          this.setState({
+            message: txt,
+          });
+        } else if (info.file.status === 'error') {
+          message.error(`${info.file.name} file upload failed.`);
+        }
+      },
+    };
   }
 
-  async uploadImg(){
-    let fileData = this.state.fileData
+  async uploadImg() {
+    let fileData = this.state.fileData;
     let data = {
       file: fileData,
     };
     const updateDictName = await uploadImg(data);
   }
 
-  componentDidMount() {
-  }
+  componentDidMount() {}
   componentwillUnmount() {
     message.destroy();
   }
 
   render() {
-     return (
-     <div className={publicStyles.public_hight} style={{color: '#fff'}}>
-       <Content>
-         <div style={{marginLeft: '80px'}} onClick={() => this.uploadImg()}>添加文件</div>
-         </Content>
-     </div>
-     );
+    return (
+      <div className={publicStyles.public_hight} style={{ color: '#fff' }}>
+        <Content>
+          <Upload {...this.uploadProps}>
+            <div style={{ marginLeft: '100px', color: '#fff' }}>
+              上传图片(仅支持jpg、bmp、png三种图片格式)
+            </div>
+          </Upload>
+        </Content>
+        <div>{this.state.message}</div>
+      </div>
+    );
   }
 }
 
